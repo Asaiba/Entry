2d00f75334d67a4394422513cd76c528eb5e6966
Cyber_001 <144014192+Asaiba@users.noreply.github.com>
2025-01-29 23:28:21 +0200

    Count.c

---
+++ Count.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int num, cout = 0, temp;
+    
+    printf("Enter a number with more than 1 digit: ");
+    scanf("%d", &num);
+    
+    if (num < 10 || num > 9999999) {
+        printf("Invalid input! Please enter a number with more than 1 digit.\n");
+        return 1;
+    }
+    
+    temp = num;
+    while (temp != 0) {
+        temp /= 10;
+        cout++;
+    }
+    
+    printf("The number %d has %d digits.\n", num, cout);
+    
+    if (cout % 2 == 0) {
+        printf("The number of digits is even.\n");
+    } else {
+        printf("The number of digits is odd.\n");
+    }
+    
+    return 0;
+}
