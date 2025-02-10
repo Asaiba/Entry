f6bb1be49059ad751076a4838e02523ce1ae3af2
Cyber_001 <144014192+Asaiba@users.noreply.github.com>
2025-02-10 12:21:49 +0200

    f

--- Fibonnaci.c
+++ Fibonnaci.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int fibonacci(int n) {
-    if (n == 0) return 0; // Base case
+    if (n == 0) return 0; 
     if (n == 1) return 1;
-    return fibonacci(n - 1) + fibonacci(n - 2); // Recursive case
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 int main() {
-    int n = 6;
-    printf("Fibonacci(%d) = %d\n", n, fibonacci(n));
+    int n;
+    printf("Enter a number u want a fibonnaci of: \n");
+    scanf("%d",&n);
+    printf("Fibonacci of (%d) = %d\n", n, fibonacci(n));
     return 0;
 }
